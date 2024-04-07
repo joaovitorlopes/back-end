52d0e65593bd921384f1041da1052c6e384fed93
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-07 18:14:42 -0400

    first commit

---
+++ src/Profile.java
@@ -0,0 +1,6 @@
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("Hello, João");
+        System.out.println("All right?");
+    }
+}
---
+++ src/Studies.java
@@ -0,0 +1,7 @@
+public class Studies {
+    public static void main(String[] args) {
+        System.out.println("Notes");
+        System.out.println("Notes 2");
+        System.out.println("I am testing the commits of the intellij");
+    }
+}
