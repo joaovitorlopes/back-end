ceb6d95224a78f38a8f52daf5ff1469edd26cdc0
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-08 22:27:18 -0400

    using strings and formatting them

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a Screen Match");
-        System.out.println("Film: John Wick 4");
+        String filmName = "John Wick 4";
 
         int releaseYear = 2023;
         System.out.println("Release year: " + releaseYear);
         boolean isIncluded = true;
         double evaluation = 8.2;
-
         double media = (9.8 + 6.3 + 8.0) / 3 ;
-        System.out.println(media);
+        System.out.println(Math.round(media));
+
+        String synopsis;
+        synopsis = """
+            %s
+            Former hitman John Wick is wanted around the world and the reward for his capture is increasing.
+            Good! (%.2f)
+            Release year: %d
+            """.formatted(filmName, evaluation, releaseYear);
+        System.out.println(synopsis);
     }
 }
