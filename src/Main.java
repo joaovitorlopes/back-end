7c47666209ee15e4eaf8ffeb83e7da63df721d31
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-08 22:10:33 -0400

    using operators in java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is a Screen Match");
+        System.out.println("Film: John Wick 4");
+
+        int releaseYear = 2023;
+        System.out.println("Release year: " + releaseYear);
+        boolean isIncluded = true;
+        double evaluation = 8.2;
+
+        double media = (9.8 + 6.3 + 8.0) / 3 ;
+        System.out.println(media);
     }
 }
