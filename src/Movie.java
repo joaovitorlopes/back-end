ecbea3fa203cc6cfa2c198979bd637a198164aa3
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-14 01:25:57 -0400

    creating a movie class model

---
+++ src/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+    String name;
+    String synopsis;
+    int durationInMinutes;
+    int releaseYear;
+    boolean isIncluded;
+    double evaluation;
+    double evaluationTotal;
+}
